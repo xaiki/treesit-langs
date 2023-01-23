55bd5ca0b7ece99f9001a88bb20632b7dfe0c1d1
Niv Sardi <xaiki@evilgiggle.com>
2023-01-23 05:08:43 -0300

    allow for cross compilation on linux for aarch64

--- treesit-langs-build.el
+++ treesit-langs-build.el
@@ -313,6 +313,7 @@ from the current state of the grammar repo, without cleanup."
         (pcase (format "%s" target)
           ;; Rust's triple -> system toolchain's triple
           ("aarch64-apple-darwin" "arm64-apple-macos11")
+          ("aarch64-unknown-linux-unknown" "aarch64-unknown-linux-unknown")
           ("nil" nil)
           (_ (error "Unsupported cross-compilation target %s" target))))
   (let* ((source (treesit-langs--source lang-symbol))
